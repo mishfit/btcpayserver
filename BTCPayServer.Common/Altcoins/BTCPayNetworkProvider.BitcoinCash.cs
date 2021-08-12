8b31a8481284ba5a06121b98f39def6845df4d5d
Mish Ochu <mishochu@mishochu.com>
2021-08-12 08:06:38 -0600

    feat: initialize bitcoin cash

---
+++ BTCPayServer.Common/Altcoins/BTCPayNetworkProvider.BitcoinCash.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using NBitcoin;
+using NBXplorer;
+
+namespace BTCPayServer
+{
+    public partial class BTCPayNetworkProvider
+    {
+        public void InitBitcoinCash()
+        {
+            var nbxplorerNetwork = NBXplorerNetworkProvider.GetFromCryptoCode("BCH");
+            Add(new BTCPayNetwork()
+            {
+                CryptoCode = nbxplorerNetwork.CryptoCode,
+                DisplayName = "Bitcoin Cash",
+                BlockExplorerLink = NetworkType == ChainName.Mainnet ? "https://blockchair.com/bitcoin-cash/transaction/{0}" :
+                                    "https://www.blockchain.com/bch-testnet/tx/{0}",
+                NBXplorerNetwork = nbxplorerNetwork,
+                UriScheme = "bitcoincash",
+                CryptoImagePath = "imlegacy/bitcoin-cash.png",
+                DefaultSettings = BTCPayDefaultSettings.GetDefaultSettings(NetworkType),
+                CoinType = NetworkType == ChainName.Mainnet ? new KeyPath("0'") : new KeyPath("1'"),
+                SupportRBF = false,
+                SupportPayJoin = false,
+                VaultSupported = false,
+                DefaultRateRules = new[]
+                {
+                    "BCH_X = BCH_BTC * BTC_X",
+                    "BCH_BTC = kraken(BCH_BTC)"
+                },
+            });
+        }
+    }
+}
