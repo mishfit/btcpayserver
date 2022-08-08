76d659a3586d1867da900731e6d1119bb34403bd
Mish Ochu <mishochu@mishochu.com>
2022-08-08 07:18:52 -0600

    log out instance status of nbxplorerNetwork

--- BTCPayServer.Common/Altcoins/BTCPayNetworkProvider.ECash.cs
+++ BTCPayServer.Common/Altcoins/BTCPayNetworkProvider.ECash.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using Microsoft.Extensions.Logging;
 using NBitcoin;
 using NBXplorer;
 
@@ -6,9 +7,23 @@ namespace BTCPayServer
 {
     public partial class BTCPayNetworkProvider
     {
+        private readonly ILogger<BTCPayNetworkProvider> _logger;
         public void InitECash()
         {
             var nbxplorerNetwork = NBXplorerNetworkProvider.GetFromCryptoCode("XEC");
+
+            if (nbxplorerNetwork == null)
+            {
+                _logger.LogError($"{nameof(nbxplorerNetwork)} is null.");
+            }
+
+            var defaultSettings = BTCPayDefaultSettings.GetDefaultSettings(NetworkType);
+            if (defaultSettings == null)
+            {
+                _logger.LogError($"{nameof(defaultSettings)} is null.");
+            }
+
+
             Add(new BTCPayNetwork()
             {
                 CryptoCode = nbxplorerNetwork.CryptoCode,
@@ -17,7 +32,7 @@ namespace BTCPayServer
                                     "https://texplorer.bitcoinabc.org/tx/{0}",
                 NBXplorerNetwork = nbxplorerNetwork,
                 CryptoImagePath = "imlegacy/ecash.png",
-                DefaultSettings = BTCPayDefaultSettings.GetDefaultSettings(NetworkType),
+                DefaultSettings = defaultSettings,
                 CoinType = NetworkType == ChainName.Mainnet ? new KeyPath("899'") : new KeyPath("0'"),
                 SupportRBF = false,
                 SupportPayJoin = false,
